a7b1f5d74ed7a0cf2e7e6f0e2bd6f3707fb20c6c
Prabhu Ramachandran <prabhu@enthought.com>
2015-04-06 21:16:33 +0530

    Add slides on virtualenv, packaging, CI etc.

---
+++ slides/devtools/code/my_project/README.md
@@ -0,0 +1,4 @@
+Simple demo project.
+
+Primarily used to demonstrate writing the simplest setup.py.
+
---
+++ slides/devtools/code/my_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sees",
+    version="0.1",
+    description="Utility code for SEES course",
+    author="FOSSEE developers",
+    author_email="python@fossee.in",
+    packages=find_packages(),
+)
